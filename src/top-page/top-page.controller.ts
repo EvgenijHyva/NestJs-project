0c77e77b1e0d2c854aa823d48cfcc9c91fe5241b
Evgeny Hyvärinen <evgeny.hyvarinen@gmail.com>
2022-10-31 12:16:51 +0200

    guards

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, 
+	Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
 import { CreateTopPageDto } from './dto/create-top-page.dto';
 import { FindTopPageDto } from './dto/find-top-page.dto';
@@ -9,11 +11,13 @@ import { TopPageService } from './top-page.service';
 export class TopPageController {
 	constructor(private readonly topPageService: TopPageService) {}
 
+	@UseGuards(JwtAuthGuard)
 	@Post('save')
 	async create(@Body() dto: CreateTopPageDto) { 
 		return this.topPageService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('get/:id')
 	async get(@Param('id', IdValidationPipe) id: string) {
 		const page = await this.topPageService.findById(id);
@@ -23,6 +27,7 @@ export class TopPageController {
 		return page;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('getByAlias/:alias')
 	async getByAlias(@Param('alias') alias: string) {
 		const page = await this.topPageService.findByAlias(alias);
@@ -32,6 +37,7 @@ export class TopPageController {
 		return page;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete('delete/:id')
 	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedPage = await this.topPageService.deleteById(id);
@@ -40,6 +46,7 @@ export class TopPageController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Patch(':id')
 	async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: CreateTopPageDto) {
 		const updatedPage = await this.topPageService.updateById(id, dto);
